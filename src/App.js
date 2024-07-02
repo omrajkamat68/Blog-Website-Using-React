a228949919cc92b48e4ae69dc07771f0700cee0b
omrajkamat68 <ob2614@srmist.edu.in>
2024-07-02 09:37:14 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Blogs from './Components/Blogs';
+import Homepage from './Components/Homepage';
+import Navbar from "./Components/navbar";
+import { selectSignedIn } from './features/userSlice';
 import './App.css';
 
+
 function App() {
+
+  const isSignedIn = useSelector(selectSignedIn);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Homepage />
+      {isSignedIn && <Blogs />}
     </div>
   );
-}
+};
 
 export default App;
